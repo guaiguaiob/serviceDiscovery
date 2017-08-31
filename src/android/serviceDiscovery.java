4289f97ea809b48f54708e7fd493c02941a1da92
Howard <zhanghaowu@gmail.com>
2017-08-31 17:22:49 +0800

    v4

--- src/android/serviceDiscovery.java
+++ src/android/serviceDiscovery.java
@@ -20,26 +20,31 @@ public class serviceDiscovery extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, final CallbackContext callbackContext) throws JSONException {
+         callbackContext.success(action);
+         return true;
         if (action.equals("getNetworkServices")) {
             JSONObject options = new JSONObject();
             try {
                 options.put("urn", "tagfans-com:device:tbox:1");
                 options.put("time", 10);
                  callbackContext.success('true');
+                 return true;
             } catch(Exception e) {
                 callbackContext.success('fail');
                 e.printStackTrace();
-                return;
+                return true;
             }
             DevDiscovery task = new DevDiscovery(MainActivity.this) {
                 @Override
                 protected void onProgressUpdate(Integer... values) {
                     callbackContext.success(this.getDevices());
+                    return true;
                 }
 
                 @Override
                 protected void onPostExecute(HashSet<String> devices) {
                     callbackContext.success(devices);
+                    return true;
                 }
 
             };
