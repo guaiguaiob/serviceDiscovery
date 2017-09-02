ec98dc8d5fb3b44fa5702d6639c241c08ce3a906
Howard <zhanghaowu@gmail.com>
2017-09-03 03:48:04 +0800

    v14

--- src/android/serviceDiscovery.java
+++ src/android/serviceDiscovery.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.*;
 import android.util.Log;
 
 import org.apache.cordova.*;
