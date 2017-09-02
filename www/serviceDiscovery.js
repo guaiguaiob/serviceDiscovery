4bafaf7ce589dce22a026aacc8522b976492a0b2
Howard <zhanghaowu@gmail.com>
2017-09-03 04:36:03 +0800

    v12

--- www/serviceDiscovery.js
+++ www/serviceDiscovery.js
@@ -5,7 +5,6 @@ module.exports = serviceDiscovery;
 
 serviceDiscovery.getNetworkServices = function (successCallback, errorCallback) {
     exec(successCallback, errorCallback, "serviceDiscovery", "getNetworkServices", []);
-    console.log(1);
     return true;
 };
 
