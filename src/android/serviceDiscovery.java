6c1b1c6ce777d8ce8cec6bb95c4a295da6250f7a
Howard <zhanghaowu@gmail.com>
2017-09-03 04:15:46 +0800

    V21

--- src/android/serviceDiscovery.java
+++ src/android/serviceDiscovery.java
@@ -34,12 +34,12 @@ public class serviceDiscovery extends CordovaPlugin {
             DevDiscovery task = new DevDiscovery(cordova.getActivity()) {
                 @Override
                 protected void onProgressUpdate(Integer... values) {
-                    callbackContext.success('OK');
+                    callbackContext.success("OK");
                 }
 
                 @Override
                 protected void onPostExecute(HashSet<String> devices) {
-                    callbackContext.success('OK');
+                    callbackContext.success("OK");
                 }
 
             };
